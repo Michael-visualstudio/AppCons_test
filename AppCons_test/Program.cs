ca8c97c352f6d8c861b73789de7784f922f4241d
MICHAEL ARIEL ADAME BERNAL <michael.adame@pucese.edu.ec>
2020-05-25 09:31:44 -0500

    ejecucion secuencial

--- AppCons_test/Program.cs
+++ AppCons_test/Program.cs
@@ -11,7 +11,15 @@ namespace AppCons_test
         static void Main(string[] args)
         {
             Console.WriteLine("Hola mundo c#");
+
+            //ejemplo de los pilares de la programacion estructurada
+            //secuencias
+            int a;
+            a = 10;
+            int b = 20;
+            Console.WriteLine("la multiplicacion de {0} x {1}", a, b, (a * b));
             Console.WriteLine("Pulse una tecla para continuar...");
+            Console.ReadKey();
         }
     }
 }
