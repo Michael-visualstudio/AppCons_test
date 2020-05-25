de52281aac14bc603c7f3e0bf1df41c994939587
MICHAEL ARIEL ADAME BERNAL <michael.adame@pucese.edu.ec>
2020-05-25 10:00:53 -0500

    bucle while

--- AppCons_test/Program.cs
+++ AppCons_test/Program.cs
@@ -25,6 +25,15 @@ namespace AppCons_test
             else
                 Console.WriteLine(" {0} es mayor", b);
 
+            
+
+            //bucle while
+            int j = 1;
+            while (j <= 10)
+            {
+                Console.WriteLine("{0} Bienvenidos a GIT ", j);
+                j++;
+            }
             Console.WriteLine("Pulse una tecla para continuar...");
             Console.ReadKey();
         }
